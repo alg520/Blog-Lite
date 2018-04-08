fb8026a635a4d5b7916fa146186da751755d10f5
shivam <shvmsngh91@gmail.com>
2018-04-08 21:08:33 +0530

    add di modules wiring for posts

--- app/src/main/java/com/shivamsingh/blog_lite/di/modules/ActivityBindingModule.kt
+++ app/src/main/java/com/shivamsingh/blog_lite/di/modules/ActivityBindingModule.kt
@@ -1,7 +1,16 @@
 package com.shivamsingh.blog_lite.di.modules
 
+import com.aasaanjobs.partnerinternal.di.scopes.PerActivity
+import com.shivamsingh.blog_lite.ui.features.posts.PostsActivity
+import com.shivamsingh.blog_lite.ui.features.posts.module.PostsDataModule
+import com.shivamsingh.blog_lite.ui.features.posts.module.PostsModule
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
 
 @Module
-class ActivityBindingModule {
+abstract class ActivityBindingModule {
+
+    @PerActivity
+    @ContributesAndroidInjector(modules = [(PostsModule::class), (PostsDataModule::class)])
+    abstract fun postsActivity(): PostsActivity
 }
---
+++ app/src/main/java/com/shivamsingh/blog_lite/ui/features/posts/module/PostsModule.kt
@@ -0,0 +1,26 @@
+package com.shivamsingh.blog_lite.ui.features.posts.module
+
+import com.aasaanjobs.partnerinternal.di.scopes.PerActivity
+import com.aasaanjobs.partnerinternal.di.scopes.PerFragment
+import com.shivamsingh.blog_lite.ui.features.posts.PostsContract
+import com.shivamsingh.blog_lite.ui.features.posts.PostsFragment
+import com.shivamsingh.blog_lite.ui.features.posts.PostsPresenter
+import dagger.Binds
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class PostsModule {
+
+    @PerFragment
+    @ContributesAndroidInjector
+    internal abstract fun postsFragment(): PostsFragment
+
+    @Binds
+    @PerFragment
+    abstract fun postsView(postsFragment: PostsFragment): PostsContract.View
+
+    @Binds
+    @PerActivity
+    abstract fun postsPresenter(presenter: PostsPresenter): PostsContract.Presenter
+}
