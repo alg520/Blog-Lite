f6958d0201c72dbc9aa47f4bfbc9adf7a7dfcace
shivam <shvmsngh91@gmail.com>
2018-04-10 00:58:45 +0530

    add juni test dependencies and clean test package

---
+++ app/src/test/java/com/shivamsingh/blog_lite/ui/features/posts/PostsPresenterTest.kt
@@ -0,0 +1,25 @@
+package com.shivamsingh.blog_lite.ui.features.posts
+
+import org.junit.Test
+
+class PostsPresenterTest {
+    @Test
+    fun takeView() {
+    }
+
+    @Test
+    fun fetchPosts() {
+    }
+
+    @Test
+    fun onPostSelection() {
+    }
+
+    @Test
+    fun getFetchPostsUseCase() {
+    }
+
+    @Test
+    fun getPostMapper() {
+    }
+}
