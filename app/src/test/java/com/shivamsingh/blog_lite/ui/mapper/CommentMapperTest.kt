6e171e6ee715650039edc90e56a1e6f32071e49a
shivam <shvmsngh91@gmail.com>
2018-04-11 21:30:20 +0530

    add comment from model layer to comment entity of presentation layer mapper test.

---
+++ app/src/test/java/com/shivamsingh/blog_lite/ui/mapper/CommentMapperTest.kt
@@ -0,0 +1,40 @@
+package com.shivamsingh.blog_lite.ui.mapper
+
+import com.shivamsingh.blog_lite.domain.model.Comment
+import com.shivamsingh.blog_lite.util.BaseTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class CommentMapperTest : BaseTest() {
+
+    val avatarUrlWithEmailPlaceHolder = "http://testdomain/200/%s.png"
+    private lateinit var commentMapper: CommentMapper
+
+    @Before
+    fun setup() {
+        commentMapper = CommentMapper(avatarUrlWithEmailPlaceHolder)
+    }
+
+    @Test
+    fun `comment should get mapped to comment entity with avatar`() {
+        // Arrange
+        val id = 1
+        val name = "shivam"
+        val email = "shvmsngh91@gmail.com"
+        val body = "some test comment"
+
+        val comment = Comment(id, name, email, body)
+
+        // When
+        val commentEntity = commentMapper.map(comment)
+
+        // Then
+
+        assertThat(commentEntity.id).isEqualTo(id)
+        assertThat(commentEntity.name).isEqualTo(name)
+        assertThat(commentEntity.email).isEqualTo(email)
+        assertThat(commentEntity.body).isEqualTo(body)
+        assertThat(commentEntity.avatar).isEqualTo(avatarUrlWithEmailPlaceHolder.format(email))
+    }
+}
